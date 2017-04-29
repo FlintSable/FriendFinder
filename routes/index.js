94e2d000847d1a32ea034e6e25dfbd13aa1ba099
Nicholas N <nnlaor@gmail.com>
2017-04-28 20:13:32 -0700

    updating

--- routes/index.js
+++ routes/index.js
@@ -3,34 +3,42 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	// the seccond paramter could be a function
-	indexData = {
-		title: 'Landing Express',
-		message: 'And to the Jungle'
-	};
-  res.render('index', { title: 'Landing Express' , indexData});
+    // the seccond paramter could be a function
+    indexData = {
+        title: 'Friend Finder',
+        message: 'And to the Jungle'
+    };
+    res.render('index', indexData);
 });
 
 
 // create post requests from the client to test
-router.post('/', function(req, res, next){
-	// post encodes
-	// find out what that means
-	// action='/'
-	// html has the method='POST'
-	// action="/?method=PUT"
+router.post('/', function(req, res, next) {
+    // post encodes
+    // find out what that means
+    // action='/'
+    // html has the method='POST'
+    // action="/?method=PUT"
 
-	res.redirect('/');
-	res.json();
+    res.redirect('/');
+    res.json();
 });
 
 
 router.get('/survey', function(req, res, next) {
+	surveyData = {
+		title: 'Date Survey',
+		message: 'ONLINE DATING',
+		digits: [1, 2, 3, 4, 5]
+	};
 
-  res.render('survey', { title: 'Date Survey' , message: 'ONLINE DATING'});
+    res.render('survey', surveyData);
 });
 
 
+router.get('/results', function(req, res, next){
+
+
+});
+
 module.exports = router;
-
-
