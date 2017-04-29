0e7e86d75e1580f5e501f1ecc59d86adb3f3f238
Nicholas N <nnlaor@gmail.com>
2017-04-29 09:20:15 -0700

    breakfast

---
+++ api/data.js
@@ -0,0 +1,44 @@
+var poke = [{
+    "name": "Bulbasaur",
+    "photo": "http://cdn.bulbagarden.net/upload/e/ec/001MS.png",
+    "scores": [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+}, {
+    "name": "Ivysaur",
+    "photo": "http://cdn.bulbagarden.net/upload/6/6b/002MS.png",
+    "scores": [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+}, {
+    "name": "Venusaur",
+    "photo": "http://cdn.bulbagarden.net/upload/d/df/003MS.png",
+    "scores": [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+}, {
+    "name": "Charmander",
+    "photo": "http://cdn.bulbagarden.net/upload/b/bb/004MS.png",
+    "scores": [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+}, {
+    "name": "Charmeleon",
+    "photo": "http://cdn.bulbagarden.net/upload/d/dc/005MS.png",
+    "scores": [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+}, {
+    "name": "Charizard",
+    "photo": "http://cdn.bulbagarden.net/upload/0/01/006MS.png",
+    "scores": [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+}, {
+    "name": "Squirtle",
+    "photo": "http://cdn.bulbagarden.net/upload/9/92/007MS.png",
+    "scores": [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+}, {
+    "name": "Wartortle",
+    "photo": "http://cdn.bulbagarden.net/upload/f/f3/008MS.png",
+    "scores": [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+}, {
+    "name": "Blastoise",
+    "photo": "http://cdn.bulbagarden.net/upload/c/cb/009MS.png",
+    "scores": [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+}, {
+    "name": "Pikachu",
+    "photo": "http://cdn.bulbagarden.net/upload/0/0f/025MS.png",
+    "scores": [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+
+}];
+
+module.exports = poke;
